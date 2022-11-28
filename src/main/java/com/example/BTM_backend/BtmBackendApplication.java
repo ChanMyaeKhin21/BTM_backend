c05a9b8e7c4fad413bd469e75037c1b08c1c6237
Chan Myae Khin <mcchanmyae.21@gmail.com>
2022-11-28 13:14:59 +0000

    configurations

---
+++ src/main/java/com/example/BTM_backend/BtmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.BTM_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BTM_backend/BtmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BTM_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
